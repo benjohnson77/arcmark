402f21db010fc7851ab5f90bd5bdbcf1febaba08
benjohnson77 <bjohnson@ubersoftware.com>
2014-07-14 14:01:44 -0500

    arcs added and working

--- source/js/all.js
+++ source/js/all.js
@@ -4,19 +4,25 @@
 
 
 $(document).ready(function(){
-	var top = $('#overview').position().top;
-	var height = $('#overview').height();
-	var y = top+height ;
-	var x = $('#overview').width();
-	var center = x/2;
-	var R = 100;
-	var paper_1 = Raphael(0, top, x, height);
-	
-	var xmod = 60;	
-	var arcs = paper_1.path(['M', center, height, "A", 1000, 1600, 1, 1, 1, -90, 90]);
-	var arcs = paper_1.path(['M', center+xmod+30, height, "A",1000, 1600, 1, 1, 1, -90, 90]);
-	var arcs = paper_1.path(['M', center+xmod+30, height, "A",1000, 1600, 1, 1, 1, -90, 90]);
-	var arcs = paper_1.path(['M', center+xmod+30, height, "A",1000, 1600, 1, 1, 1, -90, 90]);
-	var arcs = paper_1.path(['M', center+xmod+30, height, "A",1000, 1600, 1, 1, 1, -90, 90]);
-	var arcs = paper_1.path(['M', center+xmod+30, height, "A",1000, 1600, 1, 1, 1, -90, 90]);
+
+	function arcs(n){
+		var top = $('#overview').position().top;
+		var height = $('#overview').height();
+		var y = top+height ;
+		var x = $('#overview').width();
+		var center = x/2;
+		var R = 100;
+
+		var paper_1 = Raphael(0, top, x, height);
+		xmod = 60;
+		for (i = 0; i < n; i++) { 
+			var xmod = xmod + 30; 
+		
+			var arcs = paper_1.path(['m', center+xmod, height, 'a', 1100-xmod*2, 1600-xmod*2, 1, 1, 1, -90, 90]);
+			arcs.attr("stroke", "#ccc");
+			//arcs.animate({path: arcs}, 1000);
+		};
+	};
+
+	var a = arcs(6);
 });
